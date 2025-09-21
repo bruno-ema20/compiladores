e4e409043999041663a1212b01caa63954ea6c98
bruno-ema20 <bruno-ema20@fpuna.edu.py>
2025-09-21 18:23:55 -0300

    Update tokens.h

--- tokens.h
+++ tokens.h
@@ -1,1 +1,22 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// Definición de todos los tipos de tokens válidos
+typedef enum {
+    L_CORCHETE,     // [
+    R_CORCHETE,     // ]
+    L_LLAVE,        // {
+    R_LLAVE,        // }
+    COMA,           // ,
+    DOS_PUNTOS,     // :
+    LITERAL_CADENA, // "texto"
+    LITERAL_NUM,    // 123, 3.14, 2e10
+    PR_TRUE,        // true / TRUE
+    PR_FALSE,       // false / FALSE
+    PR_NULL,        // null / NULL
+    EOF_TOKEN,      // Fin de archivo
+    ERROR_TOKEN     // Token no válido
+} TokenType;
+
+#endif
 
